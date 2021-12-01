677ad11810b329e63bf760b68b50a8572b5aab6a
Johann Hercovitz <johann.hercovitz@gmail.com>
2021-12-01 12:14:15 +0300

    display cards ok

--- js/search.js
+++ js/search.js
@@ -5,7 +5,7 @@ const searchInput = document.getElementById('recherche')
 searchInput.addEventListener('input', function(e) {
     console.log(e.target.value)
 
-    const recipe = [displayRecipe]
+    const recipe = [recipeArray]
     const filteredRecipe = recipe.filter(function(e) {
         return searchInput
     });
@@ -13,50 +13,173 @@ searchInput.addEventListener('input', function(e) {
 
 })
 
-function displayRecipe(name, description, ingredients, time) {
-    // manipuler la page pour y ajouter la carte de recette
-    let recipeCard =
-        `<div class="recettes">
+
+// function displayRecipe(name, description, ingredients, time) {
+//     // manipuler la page pour y ajouter la carte de recette
+//     let recipeCard =
+//         `<div class="recettes">
+//         <div class="gris_fonce"></div>
+//         <p class="p1">${name}</p>
+//         <i class="far fa-clock item">
+//         <p class="p4">${time}</p></i>
+//         <div class="ingredients_recette">
+//         <p class="p2">${ingredients.map(ingredients => `${ingredients.ingredient}: ${ingredients.quantity} ${ingredients.unit}<br>`)}</p>
+//         </div>
+//         <div class="description">
+//         <p class="p3">${description}</p>
+//         </div>
+//         </div>`;
+//     document.querySelector('section').innerHTML = recipeCard;
+//     console.log(recipeCard)
+// }
+
+const recipeArray = [
+    // displayRecipe("Limonade de Coco", "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée", [{ "ingredient": "Lait de coco", "quantity": 400, "unit": "ml" },
+    //     { "ingredient": "Jus de citron", "quantity": 2 },
+    //     { "ingredient": "Crème de coco", "quantity": 2, "unit": "cuillères à soupe" },
+    //     { "ingredient": "Sucre", "quantity": 30, "unit": "grammes" },
+    //     { "ingredient": "Glaçons" }
+    // ], "10 min"),
+
+    // displayRecipe("Poisson Cru à la tahitienne", "Découper le thon en dés, mettre dans un plat et recouvrir de jus de citron vert (mieux vaut prendre un plat large et peu profond). Laisser reposer au réfrigérateur au moins 2 heures. (Si possible faites-le le soir pour le lendemain. Après avoir laissé mariner le poisson, coupez le concombre en fines rondelles sans la peau et les tomates en prenant soin de retirer les pépins. Rayer la carotte. Ajouter les légumes au poissons avec le citron cette fois ci dans un Saladier. Ajouter le lait de coco. Pour ajouter un peu plus de saveur vous pouver ajouter 1 à 2 cuillères à soupe de Crème de coco", [{ "ingredient": "Thon Rouge (ou blanc)", "quantity": 200, "unit": "grammes" },
+    //     { "ingredient": "Concombre", "quantity": 1 },
+    //     { "ingredient": "Tomate", "quantity": 2 },
+    //     { "ingredient": "Carotte", "quantite": 1 },
+    //     { "ingredient": "Citron Vert", "quantity": 5 },
+    //     { "ingredient": "Lait de Coco", "quantity": 100, "unit": "ml" }
+    // ], "60 min"),
+
+    // displayRecipe("Poulet coco réunionnais", "Découper le poulet en morceaux, les faire dorer dans une cocotte avec de l'huile d'olive. Salez et poivrez. Une fois doré, laisser cuire en ajoutant de l'eau. Au bout de 30 minutes, ajouter le coulis de tomate, le lait de coco ainsi que le poivron et l'oignon découpés en morceaux. Laisser cuisiner 30 minutes de plus. Servir avec du riz", [{ "ingredient": "Poulet", "quantity": 1 },
+    //     { "ingredient": "Lait de coco", "quantity": 400, "unit": "ml" },
+    //     { "ingredient": "Coulis de tomate", "quantity": 25, "unit": "cl" },
+    //     { "ingredient": "Oignon", "quantity": 1 },
+    //     { "ingredient": "Poivron rouge", "quantity": 1 },
+    //     { "ingredient": "Huile d'olive", "quantity": 1, "unit": "cuillères à soupe" }
+    // ], "80 min")
+    {
+        "id": 1,
+        "name": "Limonade de Coco",
+        "servings": 1,
+        "ingredients": [{
+                "ingredient": "Lait de coco",
+                "quantity": 400,
+                "unit": "ml"
+            },
+            {
+                "ingredient": "Jus de citron",
+                "quantity": 2
+            },
+            {
+                "ingredient": "Crème de coco",
+                "quantity": 2,
+                "unit": "cuillères à soupe"
+            },
+            {
+                "ingredient": "Sucre",
+                "quantite": 30,
+                "unit": "grammes"
+            },
+            {
+                "ingredient": "Glaçons"
+            }
+        ],
+        "time": "10 min",
+        "description": "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée",
+        "appliance": "Blender",
+        "ustensils": ["cuillère à Soupe", "verres", "presse citron"]
+    },
+    {
+        "id": 2,
+        "name": "Poisson Cru à la tahitienne",
+        "servings": 2,
+        "ingredients": [{
+                "ingredient": "Thon Rouge (ou blanc)",
+                "quantity": 200,
+                "unit": "grammes"
+            },
+            {
+                "ingredient": "Concombre",
+                "quantity": 1
+            },
+            {
+                "ingredient": "Tomate",
+                "quantity": 2
+            },
+            {
+                "ingredient": "Carotte",
+                "quantite": 1
+            },
+            {
+                "ingredient": "Citron Vert",
+                "quantity": 5
+            },
+            {
+                "ingredient": "Lait de Coco",
+                "quantity": 100,
+                "unit": "ml"
+            }
+        ],
+        "time": "60 min",
+        "description": "Découper le thon en dés, mettre dans un plat et recouvrir de jus de citron vert (mieux vaut prendre un plat large et peu profond). Laisser reposer au réfrigérateur au moins 2 heures. (Si possible faites-le le soir pour le lendemain. Après avoir laissé mariner le poisson, coupez le concombre en fines rondelles sans la peau et les tomates en prenant soin de retirer les pépins. Rayer la carotte. Ajouter les légumes au poissons avec le citron cette fois ci dans un Saladier. Ajouter le lait de coco. Pour ajouter un peu plus de saveur vous pouver ajouter 1 à 2 cuillères à soupe de Crème de coco",
+        "appliance": "Saladier",
+        "ustensils": ["presse citron"]
+    }, {
+        "id": 3,
+        "name": "Poulet coco réunionnais",
+        "servings": 4,
+        "ingredients": [{
+                "ingredient": "Poulet",
+                "quantity": 1
+            },
+            {
+                "ingredient": "Lait de coco",
+                "quantity": 400,
+                "unit": "ml"
+            },
+            {
+                "ingredient": "Coulis de tomate",
+                "quantity": 25,
+                "unit": "cl"
+            },
+            {
+                "ingredient": "Oignon",
+                "quantity": 1
+            },
+            {
+                "ingredient": "Poivron rouge",
+                "quantity": 1
+            },
+            {
+                "ingredient": "Huile d'olive",
+                "quantity": 1,
+                "unit": "cuillères à soupe"
+            }
+        ],
+        "time": "80 min",
+        "description": "Découper le poulet en morceaux, les faire dorer dans une cocotte avec de l'huile d'olive. Salez et poivrez. Une fois doré, laisser cuire en ajoutant de l'eau. Au bout de 30 minutes, ajouter le coulis de tomate, le lait de coco ainsi que le poivron et l'oignon découpés en morceaux. Laisser cuisiner 30 minutes de plus. Servir avec du riz",
+        "appliance": "Cocotte",
+        "ustensils": ["couteau"]
+    },
+];
+let recipeCard = ``;
+recipeArray.forEach(function(displayRecipe) {
+            recipeCard =
+                recipeCard +
+                `<div class="recettes">
         <div class="gris_fonce"></div>
-        <p class="p1">${name}</p>
+        <p class="p1">${displayRecipe.name}</p>
         <i class="far fa-clock item">
-        <p class="p4">${time}</p></i>
+        <p class="p4">${displayRecipe.time}</p></i>
         <div class="ingredients_recette">
-        <p class="p2">${ingredients.map(ingredients => `${ingredients.ingredient}: ${ingredients.quantity} ${ingredients.unit}<br>`)}</p>
+        <p class="p2">${displayRecipe.ingredients.map(ingredients => `${ingredients.ingredient}: ${ingredients.quantity} ${ingredients.unit}<br>`)}</p>
         </div>
         <div class="description">
-        <p class="p3">${description}</p>
+        <p class="p3">${displayRecipe.description}</p>
         </div>
         </div>`;
-    document.querySelector('section').innerHTML = recipeCard;
-    console.log(recipeCard)
-}
-
-displayRecipe("Limonade de Coco", "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée", [{ "ingredient": "Lait de coco", "quantity": 400, "unit": "ml" },
-    { "ingredient": "Jus de citron", "quantity": 2 },
-    { "ingredient": "Crème de coco", "quantity": 2, "unit": "cuillères à soupe" },
-    { "ingredient": "Sucre", "quantity": 30, "unit": "grammes" },
-    { "ingredient": "Glaçons" }
-], "10 min")
-
-// displayRecipe("Poisson Cru à la tahitienne", "Découper le thon en dés, mettre dans un plat et recouvrir de jus de citron vert (mieux vaut prendre un plat large et peu profond). Laisser reposer au réfrigérateur au moins 2 heures. (Si possible faites-le le soir pour le lendemain. Après avoir laissé mariner le poisson, coupez le concombre en fines rondelles sans la peau et les tomates en prenant soin de retirer les pépins. Rayer la carotte. Ajouter les légumes au poissons avec le citron cette fois ci dans un Saladier. Ajouter le lait de coco. Pour ajouter un peu plus de saveur vous pouver ajouter 1 à 2 cuillères à soupe de Crème de coco", [{ "ingredient": "Thon Rouge (ou blanc)", "quantity": 200, "unit": "grammes" },
-//     { "ingredient": "Concombre", "quantity": 1 },
-//     { "ingredient": "Tomate", "quantity": 2 },
-//     { "ingredient": "Carotte", "quantite": 1 },
-//     { "ingredient": "Citron Vert", "quantity": 5 },
-//     { "ingredient": "Lait de Coco", "quantity": 100, "unit": "ml" }
-// ], "60 min")
-
-// displayRecipe("Poulet coco réunionnais", "Découper le poulet en morceaux, les faire dorer dans une cocotte avec de l'huile d'olive. Salez et poivrez. Une fois doré, laisser cuire en ajoutant de l'eau. Au bout de 30 minutes, ajouter le coulis de tomate, le lait de coco ainsi que le poivron et l'oignon découpés en morceaux. Laisser cuisiner 30 minutes de plus. Servir avec du riz", [{ "ingredient": "Poulet", "quantity": 1 },
-//     { "ingredient": "Lait de coco", "quantity": 400, "unit": "ml" },
-//     { "ingredient": "Coulis de tomate", "quantity": 25, "unit": "cl" },
-//     { "ingredient": "Oignon", "quantity": 1 },
-//     { "ingredient": "Poivron rouge", "quantity": 1 },
-//     { "ingredient": "Huile d'olive", "quantity": 1, "unit": "cuillères à soupe" }
-// ], "80 min")
-
-
-
+        document.querySelector('section').innerHTML = recipeCard;
+        console.log(recipeCard)    
+});
 
 
 
